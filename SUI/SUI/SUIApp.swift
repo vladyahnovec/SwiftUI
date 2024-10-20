cbbb4f705a6cc123b1cf60b5dc0e54e1d65d55fd
Vlad Yahnovec <vladyahnovec@gmail.com>
2024-10-21 00:05:29 +0300

    Initialisation of the project

---
+++ SUI/SUI/SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIApp.swift
+//  SUI
+//
+//  Created by Круглич Влад on 21.10.24.
+//
+
+import SwiftUI
+
+@main
+struct SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
