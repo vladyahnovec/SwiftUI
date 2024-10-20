f86f86eeca3a8f02c37e18546977cb00f24ced6d
Vlad Yahnovec <vladyahnovec@gmail.com>
2024-10-21 01:48:46 +0300

    foregroundStyle

--- SUI/SUI/ContentView.swift
+++ SUI/SUI/ContentView.swift
@@ -9,15 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Фиолетовая рамка.")
-            .border(Color.purple, width: 4)
-        Text("Фиолетовая рамка + отступы.")
-            .padding(10)
-            .border(Color.purple, width: 4)
-        Text("Фиолетовая рамка + отступы.")
-            .border(Color.purple, width: 4)
-            .padding(10)
-            .border(Color.purple, width: 4)
+        VStack {
+            Text("Cиний текст.")
+                .foregroundStyle(.blue)
+            Text("Cиний текст.")
+                .foregroundStyle(.linearGradient(
+                    colors: [.yellow, .blue],
+                    startPoint: .top,
+                    endPoint: .bottom
+                ))
+            VStack(alignment: .leading) {
+                Label("Первый", systemImage: "1.square.fill")
+                Label("Второй", systemImage: "2.square.fill")
+                    .foregroundStyle(.secondary) // Изменяет цвет текста на вторичный (прозрачнее)
+                Label("Третий", systemImage: "3.square.fill")
+                    .foregroundStyle(.tertiary) // Изменяет цвет текста на третичный (еще прозрачнее)
+            }.foregroundStyle(.red)
+        }
     }
 }
 
