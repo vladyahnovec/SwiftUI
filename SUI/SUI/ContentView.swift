3e69dec809c951313d53994c55ddd90d3ebffbdf
Vlad Yahnovec <vladyahnovec@gmail.com>
2024-10-21 00:56:54 +0300

    aspectRatio + options

--- SUI/SUI/ContentView.swift
+++ SUI/SUI/ContentView.swift
@@ -11,14 +11,20 @@ struct ContentView: View {
     var body: some View {
         VStack{
             Image("cat")
-                .resizable(resizingMode: .stretch) // По умолчанию. Растягивает изображение по фрейму.
-                .frame(width: 200, height: 150)
-            Image("logo")
-                .resizable(resizingMode: .tile) // Изображение будет повторяться плиткой
-                .frame(width: 200, height: 150)
+                .resizable()
+                .aspectRatio(CGSize(width: 3, height: 4), contentMode: .fill) // Заполняет фрейм, возможно, обрезая изображение
+                .frame(width: 200, height: 200)
+                .border(Color.red, width: 3)
             Image("cat")
-                .resizable(capInsets: EdgeInsets(top: 50, leading: 0, bottom: 50, trailing: 0)) // Позволяет задать те стороны, которые будут и не будут растягиваться.
-                .frame(width: 200, height: 150)
+                .resizable()
+                .aspectRatio(contentMode: .fit) // Изображение сохраняет свои пропорции и вписывается в фрейм, не обрезая его. Это означает, что оно будет уменьшено или увеличено, чтобы полностью поместиться в заданные размеры.
+                .frame(width: 200, height: 200)
+                .border(Color.red, width: 3)
+            Image("cat")
+                .resizable()
+                .aspectRatio(0.25, contentMode: .fit) // Здесь задано конкретное соотношение сторон (ширина/высота = 0.25). Изображение будет вписываться в фрейм, сохраняя это соотношение.
+                .frame(width: 200, height: 200)
+                .border(Color.red, width: 3)
         }
     }
 }
