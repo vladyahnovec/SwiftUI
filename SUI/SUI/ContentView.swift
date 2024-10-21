7ef3d00a24b25a727d239779fb0f75ded6300aae
Vlad Yahnovec <vladyahnovec@gmail.com>
2024-10-21 14:28:41 +0300

    background

--- SUI/SUI/ContentView.swift
+++ SUI/SUI/ContentView.swift
@@ -10,21 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Cиний текст.")
-                .foregroundStyle(.blue)
-            Text("Cиний текст.")
-                .foregroundStyle(.linearGradient(
-                    colors: [.yellow, .blue],
-                    startPoint: .top,
-                    endPoint: .bottom
-                ))
-            VStack(alignment: .leading) {
-                Label("Первый", systemImage: "1.square.fill")
-                Label("Второй", systemImage: "2.square.fill")
-                    .foregroundStyle(.secondary) // Изменяет цвет текста на вторичный (прозрачнее)
-                Label("Третий", systemImage: "3.square.fill")
-                    .foregroundStyle(.tertiary) // Изменяет цвет текста на третичный (еще прозрачнее)
-            }.foregroundStyle(.red)
+            Image(systemName: "swift")
+                .padding()
+                .background(in: Circle())
+                .backgroundStyle(.blue.gradient)
+            HStack {}
+                .frame(width: 100, height: 100)
+                .background(.red)
         }
     }
 }
