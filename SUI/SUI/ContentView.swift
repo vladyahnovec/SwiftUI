43cc5059322df6975c5a4b7842286d4bbbcae2ce
Vlad Yahnovec <vladyahnovec@gmail.com>
2024-10-21 14:37:49 +0300

    colorInvert

--- SUI/SUI/ContentView.swift
+++ SUI/SUI/ContentView.swift
@@ -10,13 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                HStack {
-                    Text("Hi!")            }
-                    .frame(width: 100, height: 100)
-                    .background(.red)
-                    .brightness(Double(item) * 0.2) // яркость элемента
+            HStack {
+                Text("Hi!")
             }
+            .frame(width: 100, height: 100)
+            .background(.red)
+            .colorInvert() // инвертирует цвет
         }
     }
 }
