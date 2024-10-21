b3c359a610a51ed2f8e94edfe123607582e7d903
Vlad Yahnovec <vladyahnovec@gmail.com>
2024-10-21 15:01:12 +0300

    clipShape

--- SUI/SUI/ContentView.swift
+++ SUI/SUI/ContentView.swift
@@ -12,10 +12,9 @@ struct ContentView: View {
         VStack {
             Text("123456789abcdefgtrewasdf")
                 .fixedSize()
-                .frame(width: 200, height: 100)
-                .clipped() // скрывает любое содержимое, выходящее за границы макета фигуры.
-            
-
+                .frame(width: 300, height: 300)
+                .background(.red)
+                .clipShape(Circle()) // скрывает любое содержимое, выходящее за границы макета фигуры.
         }
     }
 }
